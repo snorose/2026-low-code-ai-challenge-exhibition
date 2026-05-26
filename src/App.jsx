b71cdc478093e163701b26f3fafcf5c90a49e57f
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-05-26 13:49:53 +0900

    refactor: URL 기반 프로젝트 목록/상세 전환 로직을 useProjectRouter 훅으로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,14 @@
-import { useEffect, useState } from "react";
 import { projectRoutes } from "./data/projects";
+import { useProjectRouter } from "./hooks/useProjectRouter";
 import ProjectDetail from "./pages/ProjectDetail";
 import ProjectList from "./pages/ProjectList";
 
-const getProjectSlugFromPath = () => {
-  const [, basePath, projectSlug] = window.location.pathname.split("/");
-
-  if (basePath !== "projects" || !projectSlug) {
-    return null;
-  }
-
-  return decodeURIComponent(projectSlug).toLowerCase();
-};
-
 function App() {
-  const [selectedProjectId, setSelectedProjectId] = useState(
-    getProjectSlugFromPath,
-  );
+  const { selectedProjectId, navigateToProject, navigateToList } = useProjectRouter();
+
   const selectedProject = projectRoutes.find(
     (project) => project.slug === selectedProjectId,
   );
-
-  useEffect(() => {
-    const handlePopState = () => {
-      setSelectedProjectId(getProjectSlugFromPath());
-    };
-
-    window.addEventListener("popstate", handlePopState);
-
-    return () => {
-      window.removeEventListener("popstate", handlePopState);
-    };
-  }, []);
-
-  const navigateToList = () => {
-    window.history.pushState({}, "", "/");
-    setSelectedProjectId(null);
-  };
-
-  const navigateToProject = (projectSlug) => {
-    window.history.pushState({}, "", `/projects/${projectSlug}`);
-    setSelectedProjectId(projectSlug);
-  };
 
   if (selectedProject) {
     return <ProjectDetail project={selectedProject} onBack={navigateToList} />;
